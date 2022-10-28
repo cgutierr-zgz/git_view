0a98015a00ad8249be3a8d2ebe2a951135fd1ca8
Carlos Gutiérrez <carlosgutimo.3d@gmail.com>
2022-10-28 12:05:53 +0200

    Updated based on wolfenrain new repo tutorial

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,21 +3,64 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  var statusBar: StatusBarController?
-  var popover = NSPopover.init()
-  override init() {
-    popover.behavior = NSPopover.Behavior.transient //to make the popover hide when the user clicks outside of it
-  }
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return false
-  }
-  override func applicationDidFinishLaunching(_ aNotification: Notification) {
-    let controller: FlutterViewController =
-      mainFlutterWindow?.contentViewController as! FlutterViewController
-    popover.contentSize = NSSize(width: 360, height: 360) //change this to your desired size
-    popover.contentViewController = controller //set the content view controller for the popover to flutter view controller
-    statusBar = StatusBarController.init(popover)
-    mainFlutterWindow.close() //close the default flutter window
-    super.applicationDidFinishLaunching(aNotification)
-  }
+    var popover: MainFlutterWindow!
+    var statusBarItem: NSStatusItem!
+    var positioningView: NSView?
+      
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+      
+    override func applicationDidFinishLaunching(_ aNotification: Notification) {
+        if let window = mainFlutterWindow.contentView?.window {
+            window.setContentSize(NSSize(width: 0, height: 0))
+        }
+
+        // Create a new popover and set the size and content controller
+        let popover = MainFlutterWindow()
+        popover.contentSize = NSSize(width: 500, height: 400)
+        popover.behavior = .transient
+        let flutterViewController = FlutterViewController.init()
+        RegisterGeneratedPlugins(registry: flutterViewController)
+        popover.contentViewController = flutterViewController
+        self.popover = popover
+        
+        // Create a new status item
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+
+        // Assign the action and title
+        if let button = self.statusBarItem.button {
+			// Uncomment the following line to use a custom image
+            // button.image = #imageLiteral(resourceName: "AppIcon")
+            // button.image?.size = NSSize(width: 18.0, height: 18.0)
+            // button.image?.isTemplate = true
+
+            // Uncomment the following line to use a custom title
+            button.title = "GitView"
+
+            button.action = #selector(togglePopover(_:))
+        }
+    }
+      
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                // Get the positioning view and add it to the button
+                let positioningView = NSView(frame: button.bounds)
+                positioningView.identifier = NSUserInterfaceItemIdentifier(rawValue: "positioningView")
+                button.addSubview(positioningView)
+
+                // Show the popover relative to the positioning view
+                popover.show(relativeTo: positioningView.bounds, of: positioningView, preferredEdge: .maxY)
+
+				// Uncommnet the following line to hide the popover arrow
+                button.bounds = button.bounds.offsetBy(dx: 0, dy: button.bounds.height)
+                if let popoverWindow = popover.contentViewController?.view.window {
+                    popoverWindow.setFrame(popoverWindow.frame.offsetBy(dx: 0, dy: 10), display: false)
+                }
+            }
+        }
+    }
 }
