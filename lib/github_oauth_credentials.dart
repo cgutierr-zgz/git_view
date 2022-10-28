66de46b048547a4545a8d4303f54b42fb84d4eeb
Carlos Gutiérrez <carlosgutimo.3d@gmail.com>
2022-10-28 12:14:21 +0200

    Added some config file changes

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,5 @@
+const githubClientId = String.fromEnvironment('GITHUB_CLIENT_ID');
+const githubClientSecret = String.fromEnvironment('GITHUB_CLIENT_SECRET');
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
